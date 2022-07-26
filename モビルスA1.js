ff175b969ef8931a3c540695299cbf4f9a328093
totemooisii <104827745+totemooisii@users.noreply.github.com>
2022-07-26 16:01:10 +0900

    Add files via upload

---
+++ モビルスA1.js
@@ -0,0 +1,36 @@
+//時刻の取得
+let now = new Date();
+nowTime = now.getHours() * 100 + now.getMinutes();
+
+let count = 0;
+//セッションの開始時刻
+st1 = 1602
+st2 = 1605
+st3 = 1608
+
+//表示させるセッションの判定
+function changeInfo() {
+    if (st1 <= nowTime && nowTime < st2) {
+        div1 = document.getElementById("div1")
+        div1.style.display = "block";
+    } else if (st2 <= nowTime && nowTime < st3) {
+        div1.style.display = "none";
+        div2 = document.getElementById("div2")
+        div2.style.display = "block";
+    }
+    console.log(nowTime);
+    // count = count + 1;
+    // if (count % 2 == 0) {
+    //     div2.style.display = "none";
+    //     div1 = document.getElementById("div1")
+    //     div1.style.display = "block";
+    // } else {
+    //     div1.style.display = "none";
+    //     div2 = document.getElementById("div2")
+    //     div2.style.display = "block";
+    // }
+    // console.log(count);
+}
+
+//１秒ごとに関数実行
+setInterval(changeInfo, 5000);
